e21a9eebb41222a13bced96e72dda884ad81fd8f
Nam Ngoc <namngoc231@gmail.com>
2022-12-15 08:58:12 +0700

    Add content file GitMain

--- src/main/java/com/example/gitjava/GitMain.java
+++ src/main/java/com/example/gitjava/GitMain.java
@@ -3,5 +3,6 @@ package com.example.gitjava;
 public class GitMain {
     public static void main(String[] args) {
         System.out.println("haaaaa");
+        System.out.println("hooooo");
     }
 }
